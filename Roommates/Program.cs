e4629497d21f29490a1732a9d73baf3444b3dabb
Jason Harris <lord.jtharris@gmail.com>
2023-06-12 13:09:53 -0500

    Added update and delete for rooms.

--- Roommates/Program.cs
+++ Roommates/Program.cs
@@ -2,6 +2,7 @@
 using Roommates.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Roommates
 {
@@ -128,6 +129,41 @@ namespace Roommates
                     case ("Exit"):
                         runProgram = false;
                         break;
+                    case ("Update a room"):
+                        List<Room> roomOptions = roomRepo.GetAll();
+                        foreach (Room r in roomOptions)
+                        {
+                            Console.WriteLine($"{r.Id} - {r.Name} Max Occupancy({r.MaxOccupancy})");
+                        }
+
+                        Console.Write("Which room would you like to update? ");
+                        int selectedRoomId = int.Parse(Console.ReadLine());
+                        Room selectedRoom = roomOptions.FirstOrDefault(r => r.Id == selectedRoomId);
+
+                        Console.Write("New Name: ");
+                        selectedRoom.Name = Console.ReadLine();
+
+                        Console.Write("New Max Occupancy: ");
+                        selectedRoom.MaxOccupancy = int.Parse(Console.ReadLine());
+
+                        roomRepo.Update(selectedRoom);
+
+                        Console.WriteLine("Room has been successfully updated");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Delete a room"):
+                        List<Room> rooms = roomRepo.GetAll();
+                        Console.WriteLine("Which room would you like to delete?\nPlease enter the ID of the room.");
+                        foreach(Room roomD in rooms)
+                        {
+                            Console.WriteLine($"{roomD.Id} - {roomD.Name}");
+                        }
+                        int roomDeleteId = int.Parse(Console.ReadLine());
+                        roomRepo.Delete(roomDeleteId);
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
                 }
             }
 
@@ -148,7 +184,9 @@ namespace Roommates
                 "Exit",
                 "Select a Roommate by ID",
                 "View unassigned chores",
-                "Assign a chore"
+                "Assign a chore",
+                "Update a room",
+                "Delete a room"
             };
 
             for (int i = 0; i < options.Count; i++)
--- Roommates/Repositories/RoomRepository.cs
+++ Roommates/Repositories/RoomRepository.cs
@@ -133,5 +133,44 @@ namespace Roommates.Repositories
 
             // when this method is finished we can look in the database and see the new room.
         }
+        /// <summary>
+        ///  Updates the room
+        /// </summary>
+        public void Update(Room room)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Room
+                                    SET Name = @name,
+                                        MaxOccupancy = @maxOccupancy
+                                    WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@name", room.Name);
+                    cmd.Parameters.AddWithValue("@maxOccupancy", room.MaxOccupancy);
+                    cmd.Parameters.AddWithValue("@id", room.Id);
+
+                    cmd.ExecuteNonQuery();//for when we don't want to/expect to receive a table back
+                }
+            }
+        }
+        /// <summary>
+        ///  Delete the room with the given id
+        /// </summary>
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // What do you think this code will do if there is a roommate in the room we're deleting???
+                    cmd.CommandText = "DELETE FROM Room WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
